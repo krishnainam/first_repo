1e9fd6d0a8b72763e25a51e315329534be066f25
krishna <43381398+krishnainam@users.noreply.github.com>
2023-10-28 13:34:20 +1100

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,4 +1,8 @@
 # This is my first program in the GitHub
 print("Hello World")
 y = input("How are you doing ? : ")
+positive = [good, fine, okay, ok, well, Well, Good, Fine, Okay, Ok]
+if(y == positive)
 print("Good to hear that you are doing " +y.lower())
+else
+print("I am sure you'll have a good day, Be positive!")
